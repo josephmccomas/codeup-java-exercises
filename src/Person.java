513a1f41db88cbe8bbece446ec4d8fee89c50d4d
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-02-13 15:07:38 -0600

    Classwork

--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,10 @@
 public class Person {
-    private String name;
 
     //    The class should have a constructor that accepts a `String` value and sets
 //the person's name to the passed string.
-    public Person(String name) {
-        this.name = "";
+    private Person(String name) {
+        name = "";
     }
-//
 
 
     public String getName() {
@@ -24,14 +22,10 @@ public class Person {
         System.out.println("Hello, " + name);
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     //Create a `main` method on the class that creates a new `Person` object and
 //tests the above methods.
     public void main(String[] args) {
-        Person name = new Person("Gabriel");
+        Person.name = new Person("Gabriel");
         System.out.println(getName());
     }
 }
